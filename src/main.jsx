5f1d31a9f2d3e8168d9559166f67ee4dd440bf13
Isabelle0210 <isabelleisa646@gmail.com>
2024-08-06 15:52:22 -0300

    Criação das rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider, Router } from 'react-router-dom'
+
+//pages
+import Home from './routes/Home.jsx'
+import NewPost from './routes/NewPost.jsx'
+
+//aqui eu fiz a configuração das rotas da aplicação e renderizei o componente App com o RouterProvider que recebe o router como propriedade e o router é criado com o método createBrowserRouter que recebe um array de objetos com as rotas da aplicação e cada objeto tem um elemento que é o componente que será renderizado e um children que é um array de objetos com as rotas filhas do componente pai.
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/new',
+        element: <NewPost />,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
